0997090b3b4ecd9cb88d5c3755cf4d9cdc44dd04
piacavasinni <pia.cavasinni@hotmail.it>
2020-01-29 19:09:20 +0100

    training

--- LBPprova.py
+++ LBPprova.py
@@ -1,18 +1,16 @@
 
 import os
-
+from sklearn.svm import LinearSVC
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from sys import platform
 from skimage.feature import local_binary_pattern
+from sklearn.externals import joblib
 
-'''
-# List for storing the LBP Histograms, address of images and the corresponding label
-X_test = []
-X_name = []
-#y_test = []
-'''
+
+labelList = []
+data = []
 
 # Constuct the figure for histogram
 plt.style.use("ggplot")
@@ -60,15 +58,18 @@ for file in os.listdir(dataset_path):
 ts, tr = [], []
 tr, ts = split_dataset(dataset_dict)
 train_images = []
+test_images = []
 for t in tr:
     for e in t:
-        train_images += ([dataset_path + e + '.jpg'])
+        train_images += ([e])
+for tt in ts:
+    for e in tt:
+        test_images += ([e])
 
-# For each image in the training set calculate the LBP histogram
-# and update X_test, X_name and y_test
-for train_image in train_images:
+for e in train_images:
     # Read the image
-    im = cv2.imread(train_image)
+    imagePath = dataset_path + e + '.jpg'
+    im = cv2.imread(imagePath)
     # Resize the image
     im_res = resizeImage(im)
     (h, w) = im.shape[:2]
@@ -76,93 +77,73 @@ for train_image in train_images:
 
     # Convert to grayscale as LBP works on grayscale image
     im_gray = cv2.cvtColor(im_res, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Image", im_gray)
-    cv2.waitKey(0)
+    #cv2.imshow("Image", im_gray)
+    #cv2.waitKey(0)
 
     # Number of points to be considered as neighbourers
     radius = 5
     no_points = 2 * radius
     # Uniform LBP is used
     lbp = local_binary_pattern(im_gray, no_points, radius)
-    cv2.imshow("LBP", lbp.astype("uint8"))
-    cv2.waitKey(0)
+    #cv2.imshow("LBP", lbp.astype("uint8"))
+    #cv2.waitKey(0)
 
     # Create histogram
-    ax.hist(lbp.ravel(), density=True, bins=20, range=(0, 256))
-    ax.set_xlim([0, 256])
-    ax.set_ylim([0, 0.030])
-    fig.savefig('temp.png', dpi=fig.dpi)
-    plt.show()
-    cv2.destroyAllWindows()
+    #ax.hist(lbp.ravel(), density=True, bins=20, range=(0, 256))
+    #ax.set_xlim([0, 256])
+    #ax.set_ylim([0, 0.030])
+    #fig.savefig('temp.png', dpi=fig.dpi)
+    #plt.show()
+    #cv2.destroyAllWindows()
 
     # Calculate the histogram
-    x = np.unique(lbp.ravel())
+    #x = np.unique(lbp.ravel())
+
+    (hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, no_points + 3), range=(0, radius + 2))
 
     # Normalize the histogram
-    hist = x / sum(x)
-    hist = hist.reshape(1, -1)
+    hist = hist.astype("float")
+    hist /= (hist.sum() + 1e-7 )
 
-    def extractHistogramList(image, label):
+    # 2th method
+    #hist = x / sum(x)
+    #hist = hist.reshape(1, -1)
 
-        # get the descriptor class initiated
-        histogramList = []
-        LabelList = []
+    labelList.append(e[:3])
+    data.append(hist)
+    #print(labelList)
+    #print(data)
 
-        # This mask has the same width and height a the original image and has a default value of 0 (black).
-        maskedImage = np.zeros(image.shape[:2], dtype="uint8")
-        ########### create imageROIList here ############
-
-        (h, w) = image.shape[:2]
-
-        # Divide the image into 100 pieces
-        cellSizeYdir = h / 10
-        cellSizeXdir = w / 10
-
-        # start in origo
-        x = 0
-        y = 0
-        counterInt = 0
-
-        # 10*10 = 100
-        for i in range(10):
-
-            # update this value
-            y = cellSizeYdir * (i)
-
-            x = 0  # it starts at 0 for a new row
-            for j in range(10):
-                print ("[x] inspecting imageROI %d" % (counterInt))
-                counterInt = counterInt + 1
-
-                x = cellSizeXdir * (j)
-
-                imageROI = image[y: cellSizeYdir * (i + 1), x:cellSizeXdir * (j + 1)]
-
-                # print "ystart  " + str(y) + "  yjump  " + str((cellSizeYdir * (i+1)))
-                # print "xstart  " + str(x) +  "  xjump  " + str((cellSizeXdir * (j+1)))
-
-                # grayscale and calculate histogram
-                grayImageROI = cv2.cvtColor(imageROI, cv2.COLOR_BGR2GRAY)
-                hist = lbp.describe(grayImageROI)
-
-                histogramList.append(hist)
-                LabelList.append(label)
-
-        return histogramList, LabelList
+model = LinearSVC(C=100.0, random_state=42)
+print(data)
+print(hist)
+print(labelList)
+x = model.fit(data, labelList)
+joblib.dump(model, "lbp_model.pkl")
 
 
-    histogramList, LabelList = extractHistogramList(im_gray, 'histo')
+# loop over the testing images
+for i in test_images:
+    print(i)
+    imagePath = dataset_path + i + '.jpg'
+    # load the image, convert it to grayscale, describe it,
+    # and classify it
+    imagetest = cv2.imread(imagePath)
+    graytest = cv2.cvtColor(imagetest, cv2.COLOR_BGR2GRAY)
 
-    '''
-   
+    radius = 5
+    no_points = 2 * radius
+    # Uniform LBP is used
+    lbptest = local_binary_pattern(graytest, no_points, radius)
+    (histt, _) = np.histogram(lbptest.ravel(), bins=np.arange(0, no_points + 3), range=(0, radius + 2))
 
-nrows = 1
-ncols = 1
-fig, axes = plt.subplots(nrows,ncols, squeeze=False)
-for row in range(nrows):
-    for col in range(ncols):
-        axes[row][col].imshow(cv2.cvtColor(cv2.imread(X_name[row*ncols+col]), cv2.COLOR_BGR2RGB))
-        axes[row][col].axis('off')
-        axes[row][col].set_title("{}".format(os.path.split(X_name[row*ncols+col])[1]))
-'''
+    # Normalize the histogram
+    histt = histt.astype("float")
+    histt /= (histt.sum() + 1e-7)
+    prediction = model.predict(histt.reshape(1, -1))
 
+    # display the image and the prediction
+    cv2.putText(imagetest, prediction[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                1.0, (0, 0, 255), 3)
+    cv2.imshow("Image", imagetest)
+    cv2.waitKey(0)
